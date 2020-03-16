e22c07ee2914f028f838b6c12d23f06ceeb688d7
Alex-gits <alexeynb1@gmail.com>
2020-03-16 02:11:58 +0200

    Updates

---
+++ precache-manifest.86a020aa173906b865c65491405fc44f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21525e028dfcb7cda6ced45dc3baaa8",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "dc7d38faf2b3fcebbfe7",
+    "url": "/first-react-project/static/css/main.f3fed609.chunk.css"
+  },
+  {
+    "revision": "45b91bf785a26804c594",
+    "url": "/first-react-project/static/js/2.66600e4e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/first-react-project/static/js/2.66600e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7d38faf2b3fcebbfe7",
+    "url": "/first-react-project/static/js/main.5807ac56.chunk.js"
+  },
+  {
+    "revision": "fb4b35ea2af0b7c08fc2",
+    "url": "/first-react-project/static/js/runtime-main.afe6437c.js"
+  }
+]);
